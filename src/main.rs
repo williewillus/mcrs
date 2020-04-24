1573cd424bfcfb617fb915d890e82f5cd1b27895
Vincent Lee <vincent.lee@outlook.com>
2020-04-24 14:54:18 -0500

    Move accept loop out of main

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,12 @@
 mod net;
 mod types;
 
-use log::{info, error};
-use net::connection::Connection;
-use std::net::TcpListener;
-use std::thread;
-
 fn main() -> anyhow::Result<()> {
     simple_logger::init_by_env();
-    info!("mcrs initializing");
+    log::info!("mcrs initializing");
 
-    let listener = TcpListener::bind("127.0.0.1:25565").expect("Failed to bind socket");
-    for stream in listener.incoming() {
-        let stream = stream?;
-        info!("Accepting connection to {:?}", stream.peer_addr());
-        let mut conn = Connection::new(stream);
-        thread::spawn(move || {
-            match conn.process() {
-                Ok(()) => info!("stream disconnected successfully"),
-                Err(e) => error!("error processing connection: {}", e)
-            }
-        });
-    }
+    let accept_handle = net::spawn_accept_loop()?;
+    
+    accept_handle.join().unwrap();
     Ok(())
 }
--- src/net/mod.rs
+++ src/net/mod.rs
@@ -4,3 +4,34 @@ mod login;
 mod packet;
 pub mod proto;
 mod status;
+
+use std::net::TcpListener;
+use std::thread;
+
+fn accept_loop()  {
+    let listener = TcpListener::bind("127.0.0.1:25565").expect("Failed to bind socket");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                log::info!("Accepting connection to {:?}", stream.peer_addr());
+                let mut conn = connection::Connection::new(stream);
+                thread::spawn(move || {
+                    match conn.process() {
+                        Ok(()) => log::info!("stream disconnected successfully"),
+                        Err(e) => log::error!("error processing connection: {}", e)
+                    }
+                });
+            },
+            Err(e) => {
+                log::error!("Failed to accept new connection: {}", e);
+            }
+        }
+    }
+}
+
+/// Spawns the accept loop thread and returns a handle to it
+pub fn spawn_accept_loop() -> std::io::Result<std::thread::JoinHandle<()>> {
+    thread::Builder::new()
+        .name("accept loop".to_owned())
+        .spawn(accept_loop)
+}
